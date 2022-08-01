9aa1608c1df5bea2142e2050329c4837e98591ee
sp-1999 <33665205+sp-1999@users.noreply.github.com>
2022-08-01 20:46:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 $(window).on("load", function() {
 
-	$(".loader .inner").fadeOut(20, function() {
-		$(".loader").fadeOut(10);
-	});
+	$(".loader .inner").fadeOut();
 	
 	$(".items").isotope({
 		filter: '*',
